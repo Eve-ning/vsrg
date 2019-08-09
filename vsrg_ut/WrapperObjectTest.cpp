933edfbd2c579dafdb18ec174a289366484a1144
Evening <johnchangqi@hotmail.com>
2019-08-09 20:57:02 +0800

    Add vec ops test

--- vsrg_ut/WrapperObjectTest.cpp
+++ vsrg_ut/WrapperObjectTest.cpp
@@ -4,40 +4,45 @@
 #include <NormalNote.h>
 #include <HitObjectVector.h>
 #include <iostream>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-namespace WrapperObject_
+namespace WrapperObjct_
 {		
-	// Initialize objects here
-	NormalNote nn_1 = NormalNote(100.0, 3);
-	NormalNote nn_2 = NormalNote(200.0, 4);
-
-	LongNote ln_1 = LongNote(std::make_shared<NormalNote>(NormalNote(100.0, 5)),
-							 std::make_shared<NormalNote>(NormalNote(300.0, 5)));
-	LongNote ln_2 = LongNote(std::make_shared<NormalNote>(NormalNote(200.0, 6)),
-							 std::make_shared<NormalNote>(NormalNote(400.0, 7)));
-
-	HitObjectVector hov_1 = HitObjectVector();
-	hov_1.push_back(std::make_shared<HitObject>(nn_1));
-
-
 	TEST_CLASS(HitObjectVector_)
 	{
 	public:
-		TEST_METHOD(HitObjectVector_copy)
+		// Initialize objects here
+		NormalNote nn_1 = NormalNote(100.0, 3);
+		NormalNote nn_2 = NormalNote(200.0, 4);
+
+		LongNote ln_1 = LongNote(std::make_shared<NormalNote>(NormalNote(100.0, 5)),
+								 std::make_shared<NormalNote>(NormalNote(300.0, 5)));
+		LongNote ln_2 = LongNote(std::make_shared<NormalNote>(NormalNote(200.0, 6)),
+								 std::make_shared<NormalNote>(NormalNote(400.0, 7)));
+		HitObjectVector ho_v_1 = HitObjectVector();
+		TEST_METHOD(HitObjectVector_vec_ops)
 		{
-			Assert::IsFalse(bool(NormalNote(100.0, -10)));
-			Assert::IsFalse(bool(NormalNote(-10.0, 10)));
-			Assert::IsFalse(bool(NormalNote(-10.0, -100)));
-			Assert::IsFalse(bool(NormalNote(10.0, -100)));
+			ho_v_1.push_back(std::make_shared<HitObject>(nn_1));
+			ho_v_1.push_back(std::make_shared<HitObject>(nn_2));
+
+			Assert::AreEqual(size_t(2), ho_v_1.size());
+
+			ho_v_1.pop_back();
+
+			Assert::AreEqual(size_t(1), ho_v_1.size());
+
+			ho_v_1.push_back(std::make_shared<HitObject>(ln_1));
+
+			// Check for polymorphism
+			Assert::AreEqual(size_t(2), ho_v_1.size());
+
+			ho_v_1.clear();
+
+			Assert::AreEqual(size_t(0), ho_v_1.size());
 		}
 
-		TEST_METHOD(NormalNote_equals)
-		{
-			// isOverlapping is an alias to == operator
-			Assert::IsTrue(nn_1 == nn_1);
-			Assert::IsFalse(nn_1.isOverlapping(nn_2));
-		}
 	};
-	
+
 }
+	
