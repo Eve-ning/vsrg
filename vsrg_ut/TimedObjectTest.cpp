9f8b4af6f1b2a4fe5d0ff1a76e4faffffa41621c
Evening <johnchangqi@hotmail.com>
2019-08-10 16:30:23 +0800

    Add separate test for timed Objs

---
+++ vsrg_ut/TimedObjectTest.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <LongNote.h>
+#include <NormalNote.h>
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace HitObject_
+{		
+	// Initialize objects here
+	NormalNote nn_1 = NormalNote(100.0, 3);
+	NormalNote nn_2 = NormalNote(200.0, 4);
+
+
+	TEST_CLASS(TimedObject_)
+	{
+	public:
+		TEST_METHOD(TimedObject_arithmetic)
+		{
+			// The non-assignment operation needs a sptr
+			auto nn_a = std::make_shared<TimedObject>(nn_1);
+			Assert::AreEqual(100.0, nn_a->getOffsetMSec());
+			auto nn_b = nn_a->operator+(10.0);
+			Assert::AreEqual(110.0, nn_b->getOffsetMSec());
+
+			// The assignment operation doesn't need conversion
+			nn_1 += 20.0;
+			Assert::AreEqual(120.0, nn_1.getOffsetMSec());
+		}
+
+		TEST_METHOD(TimedObject_setGet)
+		{
+			auto nn_0 = nn_1;
+			
+			// Check setters
+			nn_0.setOffsetHour(1.0);
+			Assert::AreEqual(3600000.0, nn_0.getOffsetMSec());
+			nn_0.setOffsetMin(1.0);
+			Assert::AreEqual(60000.0, nn_0.getOffsetMSec());
+			nn_0.setOffsetSec(1.0);
+			Assert::AreEqual(1000.0, nn_0.getOffsetMSec());
+			nn_0.setOffsetMSec(1.0);
+			Assert::AreEqual(1.0, nn_0.getOffsetMSec());
+
+			// Check getters
+
+			nn_0.setOffsetMSec(3600000.0);
+			Assert::AreEqual(1.0, nn_0.getOffsetHour());
+			nn_0.setOffsetMSec(3600000.0);
+			Assert::AreEqual(60.0, nn_0.getOffsetMin());
+			nn_0.setOffsetMSec(3600000.0);
+			Assert::AreEqual(3600.0, nn_0.getOffsetSec());
+
+			
+		}
+	};
+}
