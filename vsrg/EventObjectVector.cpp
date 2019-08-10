705b3e58b26a03c6772ad78b469bcc7560f16f9d
Evening <johnchangqi@hotmail.com>
2019-08-10 15:52:19 +0800

    Add get and set

--- vsrg/EventObjectVector.cpp
+++ vsrg/EventObjectVector.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "stdafx.h" 
 #include "EventObjectVector.h"
 
 
@@ -9,4 +9,13 @@ EventObjectVector::EventObjectVector()
 
 EventObjectVector::~EventObjectVector()
 {
+}
+
+std::vector<SPtrEventObject> EventObjectVector::getEventObjectVector() const {
+	return eo_v_;
+}
+
+void EventObjectVector::setEventObjectVector(std::vector<SPtrEventObject> eo_v)
+{
+	eo_v_ = eo_v;
 }
--- vsrg/EventObjectVector.h
+++ vsrg/EventObjectVector.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "TimedObjectVector.h"
+#include "EventObject.h"
 class EventObjectVector : public TimedObjectVector
 {
 public:
-	EventObjectVector();
-	~EventObjectVector();
-};
+	EventObjectVector() : eo_v_({}) {}
 
+	~EventObjectVector();
+
+	std::vector<SPtrEventObject> getEventObjectVector() const;
+	void setEventObjectVector(std::vector<SPtrEventObject> eo_v);
+private:
+	std::vector<SPtrEventObject> eo_v_;
+};
