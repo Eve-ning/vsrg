e9b881b0b6cbb2348df37e92b303853a0e5ae244
Evening <johnchangqi@hotmail.com>
2019-08-27 01:25:54 +0800

    Add Version and Creator

--- vsrg/VsrgMapParams.cpp
+++ vsrg/VsrgMapParams.cpp
@@ -1,63 +1,69 @@
 #include "stdafx.h"
 #include "VsrgMapParams.h"
 
+VsrgMapParams::VsrgMapParams(const std::string & title, const std::string & artist, const std::string & creator, const std::string & version, const std::wstring & title_w, const std::wstring & artist_w, const std::wstring & creator_w, const std::wstring & version_w) :
+	title_(title), artist_(artist), creator_(creator), version_(version),
+	title_w_(title_w), artist_w_(artist_w), creator_w_(creator_w), version_w_(version_w) {}
 
-VsrgMapParams::VsrgMapParams()
-{
-}
-
-
-
-/// This variant requires all to be filled, use the other for lazy instantiating
-
-VsrgMapParams::VsrgMapParams(const std::string & title, const std::string & artist, const std::wstring & title_w, const std::wstring & artist_w) :
-	title_(title), artist_(artist), title_w_(title_w), artist_w_(artist_w)
-{
-}
-
-
-/// This variant allows lazy wide string setting and or just leaving wide variants blank
-
-VsrgMapParams::VsrgMapParams(const std::string & title, const std::string & artist, bool set_wide) : title_w_(L""), artist_w_(L"")
-{
+VsrgMapParams::VsrgMapParams(const std::string & title, const std::string & artist, const std::string & creator, const std::string & version, bool set_wide) :
+	title_w_(L""), artist_w_(L""), creator_w_(L""), version_w_(L"") {
 	setTitle(title, set_wide);
 	setArtist(artist, set_wide);
+	setCreator(creator, set_wide);
+	setVersion(version, set_wide);
 }
 
-VsrgMapParams::~VsrgMapParams()
-{
-}
+VsrgMapParams::~VsrgMapParams() {}
 
 void VsrgMapParams::setTitle(const std::string & title, bool set_wide = false) {
 	title_ = title;
 	if (set_wide) setWideTitle(utf8ToUtf16(title));
 }
-
 std::string VsrgMapParams::getTitle() {
 	return title_;
 }
-
 void VsrgMapParams::setArtist(const std::string & artist, bool set_wide = false) {
 	artist_ = artist;
 	if (set_wide) setWideArtist(utf8ToUtf16(artist));
 }
-
 std::string VsrgMapParams::getArtist() {
 	return artist_;
 }
-
+void VsrgMapParams::setCreator(const std::string & creator, bool set_wide) {
+	creator_ = creator;
+	if (set_wide) setWideCreator(utf8ToUtf16(creator));
+}
+std::string VsrgMapParams::getCreator() {
+	return creator_;
+}
+void VsrgMapParams::setVersion(const std::string & version, bool set_wide) {
+	version_ = version;
+	if (set_wide) setWideVersion(utf8ToUtf16(version));
+}
+std::string VsrgMapParams::getVersion() {
+	return version_;
+}
+void VsrgMapParams::setWideCreator(const std::wstring & creator_w) {
+	creator_w_ = creator_w;
+}
+std::wstring VsrgMapParams::getWideCreator() {
+	return creator_w_;
+}
+void VsrgMapParams::setWideVersion(const std::wstring & version_w) {
+	version_w_ = version_w;
+}
+std::wstring VsrgMapParams::getWideVersion() {
+	return version_w_;
+}
 void VsrgMapParams::setWideTitle(const std::wstring & title_w) {
 	title_w_ = title_w;
 }
-
 std::wstring VsrgMapParams::getWideTitle() {
 	return title_w_;
 }
-
 void VsrgMapParams::setWideArtist(const std::wstring & artist_w) {
 	artist_w_ = artist_w;
 }
-
 std::wstring VsrgMapParams::getWideArtist() {
 	return artist_w_;
 }
--- vsrg/VsrgMapParams.h
+++ vsrg/VsrgMapParams.h
@@ -3,12 +3,27 @@ class VsrgMapParams
 {
 public:
 	/// This variant requires all to be filled, use the other for lazy instantiating
-	VsrgMapParams(const std::string & title, const std::string & artist,
-		const std::wstring & title_w, const std::wstring & artist_w);
+	VsrgMapParams(
+		const std::string & title, const std::string & artist,
+		const std::string & creator, const std::string & version,
+		const std::wstring & title_w, const std::wstring & artist_w,
+		const std::wstring & creator_w, const std::wstring & version_w) :
+		title_(title), artist_(artist), creator_(creator), version_(version),
+		title_w_(title_w), artist_w_(artist_w), creator_w_(creator_w), version_w_(version_w)
+	{
+	}
 
 	/// This variant allows lazy wide string setting and or just leaving wide variants blank
-	VsrgMapParams(const std::string & title, const std::string & artist,
-		bool set_wide = true);
+	VsrgMapParams(
+		const std::string & title, const std::string & artist,
+		const std::string & creator, const std::string & version,
+		bool set_wide = true) :
+		title_w_(L""), artist_w_(L""), creator_w_(L""), version_w_(L"") {
+		setTitle(title, set_wide);
+		setArtist(artist, set_wide);
+		setCreator(creator, set_wide);
+		setVersion(version, set_wide);
+	}
 
 	~VsrgMapParams();
 
@@ -16,6 +31,17 @@ public:
 	std::string getTitle();
 	void setArtist(const std::string & artist, bool set_wide = false);
 	std::string getArtist();
+	void setCreator(const std::string & creator, bool set_wide = false);
+	std::string getCreator();
+	void setVersion(const std::string & version, bool set_wide = false);
+	std::string getVersion();
+
+
+	void setWideCreator(const std::wstring & creator_w);
+	std::wstring getWideCreator();
+	void setWideVersion(const std::wstring & version_w);
+	std::wstring getWideVersion();
+
 	/// Wide Variant is for unicode storage
 	void setWideTitle(const std::wstring & title_w);
 	/// Wide Variant is for unicode storage
@@ -35,5 +61,9 @@ private:
 	std::string title_;
 	std::wstring artist_w_;
 	std::string artist_;
+	std::wstring creator_w_;
+	std::string creator_;
+	std::wstring version_w_;
+	std::string version_;
 };
 
