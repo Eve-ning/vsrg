6dea3acef08275598ae1fc592f32a4078f8a20f0
Evening <johnchangqi@hotmail.com>
2019-08-27 00:27:17 +0800

    Add helper functions for derived class

--- vsrg/VsrgMap.cpp
+++ vsrg/VsrgMap.cpp
@@ -1,8 +1,7 @@
 #include "stdafx.h"
 #include "VsrgMap.h"
 #include <iostream>
-#include <fstream>
-#include <filesystem>
+
 
 std::string VsrgMap::ho_v_tag_ = "// Hit Objects";
 std::string VsrgMap::eo_v_tag_ = "// Event Objects";
@@ -11,21 +10,48 @@ VsrgMap::VsrgMap() {}
 VsrgMap::~VsrgMap() {}
 
 void VsrgMap::saveAsVsrg(const std::string & file_path, bool overwrite) {
+
+	std::vector<std::string> file_contents = {};
+
+	file_contents.push_back(ho_v_tag_);
+	for (const std::string & ho_str : ho_v_.toExport()) {
+		file_contents.push_back(ho_str);
+	}
+	file_contents.push_back(eo_v_tag_);
+	for (const std::string & eo_str : eo_v_.toExport()) {
+		file_contents.push_back(eo_str);
+	}
+
+	writeFile(std::move(file_contents), file_path, overwrite);
+}
+
+inline std::vector<std::string> VsrgMap::readFile(const std::string & file_path) {
+	std::ifstream file_stream;
+	BOOST_ASSERT_MSG(std::filesystem::exists(file_path), "File doesn't exist.");
+
+	std::vector<std::string> file_vector;
+	std::string line = "";
+	file_stream.open(file_path);
+	while (std::getline(file_stream, line)) {
+		file_vector.push_back(line);
+	}
+
+	file_stream.close();
+
+	return std::move(file_vector);
+}
+
+void VsrgMap::writeFile(const std::vector<std::string> file_contents, const std::string & file_path, bool overwrite) {
 	if (!overwrite) {
-	BOOST_ASSERT_MSG(!std::filesystem::exists(file_path),
-						 "File already exists.");
+		BOOST_ASSERT_MSG(!std::filesystem::exists(file_path),
+			"File already exists.");
 	}
 
 	std::ofstream file_out;
 	file_out.open(file_path, std::ios::in);
 	BOOST_ASSERT_MSG(file_out.is_open(), "File failed to open.");
-	file_out << ho_v_tag_;
-	for (const std::string & ho_str : ho_v_.toExport()) {
-		file_out << ho_str;
-	}
-	file_out << eo_v_tag_;
-	for (const std::string & eo_str : eo_v_.toExport()) {
-		file_out << eo_str;
+	for (const std::string & line : file_contents) {
+		file_out << line << '\n';
 	}
 
 	file_out.close();
--- vsrg/VsrgMap.h
+++ vsrg/VsrgMap.h
@@ -10,10 +10,19 @@ public:
 	~VsrgMap();
 	
 	virtual void loadFile(const std::string & file_path) = 0;
-	virtual void saveFile(const std::string & file_path) = 0;
+	virtual void saveFile(const std::string & file_path, bool overwrite) = 0;
 
 	// This is the universal exporting function
-	virtual void saveAsVsrg(const std::string & file_path, bool overwrite = false);
+	virtual void saveAsVsrg(const std::string & file_path,
+						    bool overwrite = false) final;
+
+	/// This reads all lines from the file and coerces to vector.
+	/// These have additional assertions for error handling
+	virtual std::vector<std::string> readFile(const std::string & file_path);
+	/// This writes all lines to the file from vector
+	/// These have additional assertions for error handling
+	virtual void writeFile(const std::vector<std::string> file_contents,
+		const std::string & file_path, bool overwrite = false);
 
 	static std::string ho_v_tag_;
 	static std::string eo_v_tag_;
