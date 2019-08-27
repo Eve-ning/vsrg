9d3f1988c457b19a225a9ae0ae1efb1909b4242d
Evening <johnchangqi@hotmail.com>
2019-08-27 22:22:18 +0800

    Add all necessary members and constructors

--- vsrg/VsrgMapOsuParams.cpp
+++ vsrg/VsrgMapOsuParams.cpp
@@ -1,11 +1,48 @@
 #include "stdafx.h"
 #include "VsrgMapOsuParams.h"
 
+VsrgMapOsuParams::VsrgMapOsuParams() {}
 
-VsrgMapOsuParams::VsrgMapOsuParams()
-{
-}
+VsrgMapOsuParams::VsrgMapOsuParams(
+	double hp,
+	double od,
+	unsigned int preview_time,
+	unsigned int keys,
+	const std::wstring & bg_file_name,
+	const std::wstring & source,
+	const std::vector<std::wstring>& tags,
+	const std::vector<unsigned int>& bookmarks,
+	int beatmap_id,
+	int beatmap_set_id,
+	VsrgMapParams vsrg_map_params) :
+	VsrgMapParams(vsrg_map_params),
+	hp_(hp), od_(od), preview_time_(preview_time), keys_(keys),
+	bg_file_name_(bg_file_name), source_(source), tags_(tags),
+	bookmarks_(bookmarks), beatmap_id_(beatmap_id), beatmap_set_id_(beatmap_set_id) {}
 
+VsrgMapOsuParams::VsrgMapOsuParams(
+	double hp, double od, unsigned int preview_time, unsigned int keys,
+	const std::wstring & bg_file_name,
+	const std::wstring & source,
+	const std::vector<std::wstring>& tags,
+	const std::vector<unsigned int>& bookmarks,
+	int beatmap_id, int beatmap_set_id,
+	const std::string & title,
+	const std::string & artist,
+	const std::string & creator,
+	const std::string & version,
+	const std::wstring & title_w,
+	const std::wstring & artist_w,
+	const std::wstring & creator_w,
+	const std::wstring & version_w) :
+	VsrgMapParams(title, artist, creator, version, title_w, artist_w, creator_w, version_w),
+	hp_(hp), od_(od), preview_time_(preview_time), keys_(keys),
+	bg_file_name_(bg_file_name), source_(source), tags_(tags),
+	bookmarks_(bookmarks), beatmap_id_(beatmap_id), beatmap_set_id_(beatmap_set_id) {}
+
+VsrgMapOsuParams::VsrgMapOsuParams(double hp, double od, unsigned int keys, const std::string & title, const std::string & artist, const std::string & creator, const std::string & version, bool set_wide) :
+	VsrgMapParams(title, artist, creator, version, set_wide),
+	hp_(hp), od_(od), keys_(keys) {}
 
 VsrgMapOsuParams::~VsrgMapOsuParams()
 {
--- vsrg/VsrgMapOsuParams.h
+++ vsrg/VsrgMapOsuParams.h
@@ -1,14 +1,62 @@
 #pragma once
 #include "VsrgMapParams.h"
+
+/* Comments:
+	
+	Constructors
+	I'll avoid any default arguments in this derived class since we are essentially
+	reading off a pre-determined osu file
+
+	Downcasting
+	Safe downcasting is implemented in a constructor, other members just have to be
+	specified
+
+*/
+
 class VsrgMapOsuParams : VsrgMapParams
 {
 public:
 	VsrgMapOsuParams();
+
+	VsrgMapOsuParams(
+		double hp, double od, unsigned int preview_time, unsigned int keys,
+		const std::wstring & bg_file_name,
+		const std::wstring & source,
+		const std::vector<std::wstring> & tags,
+		const std::vector<unsigned int> & bookmarks,
+		int beatmap_id, int beatmap_set_id,
+		VsrgMapParams vsrg_map_params);
+
+	/// Custom wide string setting, use other constructor for lazy
+	VsrgMapOsuParams(
+		double hp, double od, unsigned int preview_time, unsigned int keys,
+		const std::wstring & bg_file_name,
+		const std::wstring & source,
+		const std::vector<std::wstring> & tags,
+		const std::vector<unsigned int> & bookmarks,
+		int beatmap_id, int beatmap_set_id,
+		const std::string & title, const std::string & artist,
+		const std::string & creator, const std::string & version,
+		const std::wstring & title_w, const std::wstring & artist_w,
+		const std::wstring & creator_w, const std::wstring & version_w);
+
+	/// Lazy wide string setting, they are copied from non-wide if set_wide == true
+	VsrgMapOsuParams(double hp, double od, unsigned int keys,
+		const std::string & title, const std::string & artist,
+		const std::string & creator, const std::string & version,
+		bool set_wide = true);
+
 	~VsrgMapOsuParams();
 
-private:
-	double hp;
-	double od;
-	unsigned int keys;
+	double hp_;
+	double od_;
+	unsigned int preview_time_;
+	unsigned int keys_;
+	std::wstring bg_file_name_;
+	std::wstring source_;
+	std::vector<std::wstring> tags_;
+	std::vector<unsigned int> bookmarks_;
+	int beatmap_id_;
+	int beatmap_set_id_;
 };
 
