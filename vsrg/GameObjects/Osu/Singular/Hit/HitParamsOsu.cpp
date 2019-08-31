e5fe7c1bae05f108546159bc748aa5f0b93cd0d7
Evening <johnchangqi@hotmail.com>
2019-08-31 21:09:05 +0800

    Add params as separate struct

--- vsrg/GameObjects/Osu/Singular/Hit/HitParamsOsu.cpp
+++ vsrg/GameObjects/Osu/Singular/Hit/HitParamsOsu.cpp
@@ -1,3 +1,9 @@
 #include "stdafx.h"
 #include "HitParamsOsu.h"
 
+unsigned int HitParamsOsu::indexToAxis(double index, unsigned int keys) {
+	return static_cast<unsigned int>(round(((512 * index) + 256) / keys));
+}
+unsigned int HitParamsOsu::axisToIndex(double axis, unsigned int keys) {
+	return static_cast<unsigned int>(round((axis * keys - 256) / 512));
+}
--- vsrg/GameObjects/Osu/Singular/Hit/HitParamsOsu.h
+++ vsrg/GameObjects/Osu/Singular/Hit/HitParamsOsu.h
@@ -3,5 +3,17 @@
 /// This should define the game specific params of a osu note, such as local hitsound
 struct HitParamsOsu
 {
+	static unsigned int indexToAxis(double index,
+		unsigned int keys);
+
+	static unsigned int axisToIndex(double axis,
+		unsigned int keys);
+
+	unsigned int hitsound_;
+	unsigned int sample_;
+	unsigned int addition_;
+	unsigned int custom_;
+	unsigned int volume_;
+	std::string hitsound_file_;
 };
 
