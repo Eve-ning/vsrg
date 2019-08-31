8beaa5f41f17cdf9cc14d4f234ecc4575b28f8b8
Evening <johnchangqi@hotmail.com>
2019-08-31 17:21:08 +0800

    Fix double definition

--- vsrg/Helpers/IterHelper.h
+++ vsrg/Helpers/IterHelper.h
@@ -9,10 +9,6 @@ namespace IterHelper
 	std::vector<std::string> grabBetween(
 		std::vector<std::string>::const_iterator begin,
 		std::vector<std::string>::const_iterator end
-	) {
-		std::vector<std::string> out;
-		while (begin != end) { out.push_back(*begin); begin++; }
-		return std::move(out);
-	}
+	);
 };
 
