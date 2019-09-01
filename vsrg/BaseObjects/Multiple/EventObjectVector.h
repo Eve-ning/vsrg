aca34f3f9cc52c236ac88cbbc6d9bb29e4c6116b
Evening <johnchangqi@hotmail.com>
2019-09-01 12:30:06 +0800

    Add helper casts on parent vectors

--- vsrg/BaseObjects/Multiple/EventObjectVector.h
+++ vsrg/BaseObjects/Multiple/EventObjectVector.h
@@ -1,9 +1,29 @@
 #pragma once
 #include "TimedObjectVector.h"
 #include "BaseObjects/Singular/Event/EventObject.h"
+
+#define EO_CAST(obj) std::static_pointer_cast<EventObject>(obj)
+
 class EventObjectVector : public TimedObjectVector
 {
 public:
 	EventObjectVector();
 	~EventObjectVector();
+
+	SPtrEventObject operator[](unsigned int index) const {
+		return EO_CAST(to_v_[index]);
+	}
+
+	std::vector<SPtrEventObject> getEventObjectVector() const {
+		std::vector<SPtrEventObject> eo_v;
+		for (const auto & eo : to_v_) eo_v.push_back(EO_CAST(eo));
+		return eo_v;
+	}
+
+	SPtrEventObject getEarliestObject() const {
+		return EO_CAST(TimedObjectVector::getEarliestObject());
+	}
+	SPtrEventObject getLatestObject() const {
+		return EO_CAST(TimedObjectVector::getLatestObject());
+	}
 };
--- vsrg/BaseObjects/Multiple/HitObjectVector.h
+++ vsrg/BaseObjects/Multiple/HitObjectVector.h
@@ -1,10 +1,30 @@
 #pragma once
 #include "TimedObjectVector.h"
 #include "BaseObjects/Singular/Hit/HitObject.h"
+
+#define HO_CAST(obj) std::static_pointer_cast<HitObject>(obj)
+
 class HitObjectVector : public TimedObjectVector
 {
 public:
 	HitObjectVector();
 	~HitObjectVector();
+
+	SPtrHitObject operator[](unsigned int index) const {
+		return HO_CAST(to_v_[index]);
+	}
+
+	std::vector<SPtrHitObject> getHitObjectVector() const {
+		std::vector<SPtrHitObject> ho_v;
+		for (const auto & ho : to_v_) ho_v.push_back(HO_CAST(ho));
+		return ho_v;
+	}
+
+	SPtrHitObject getEarliestObject() const {
+		return HO_CAST(TimedObjectVector::getEarliestObject());
+	}
+	SPtrHitObject getLatestObject() const {
+		return HO_CAST(TimedObjectVector::getLatestObject());
+	}
 };
 
