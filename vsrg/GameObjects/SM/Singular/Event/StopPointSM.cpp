1e3bf046326c4206504f018bb4895995807f3e78
Evening <johnchangqi@hotmail.com>
2019-09-30 23:05:20 +0800

    Transform StopPoint to adhere to library unit_scale standard

--- vsrg/GameObjects/SM/Singular/Event/StopPointSM.cpp
+++ vsrg/GameObjects/SM/Singular/Event/StopPointSM.cpp
@@ -1,29 +1,29 @@
 #include "stdafx.h"
 #include "StopPointSM.h"
 
-StopPointSM::StopPointSM(double offset_m_sec, double length_sec) :
-	EventObject(offset_m_sec), length_sec_(length_sec) {}
+StopPointSM::StopPointSM(double offset_m_sec, double length, double unit_scale) :
+	EventObject(offset_m_sec), length_m_sec_(length / unit_scale) {}
 
 inline std::string StopPointSM::asNative() const {
-	return "=" + std::to_string(length_sec_);
+	return "=" + std::to_string(length_m_sec_ * UnitScale::second);
 }
 
-double StopPointSM::getLength() const {
-	return length_sec_;
+double StopPointSM::getLength(double unit_scale) const {
+	return length_m_sec_ * unit_scale;
 }
 
-void StopPointSM::setLength(double length) {
-	length_sec_ = length;
+void StopPointSM::setLength(double length, double unit_scale) {
+	length_m_sec_ = length / unit_scale;
 }
 
-double StopPointSM::getEnd() const {
-	return getOffset() + length_sec_;
+double StopPointSM::getEnd(double unit_scale) const {
+	return getOffset(unit_scale) + (length_m_sec_ * unit_scale);
 }
 
-void StopPointSM::setEnd(double end) {
-	length_sec_ = end - getOffset();
+void StopPointSM::setEnd(double end, double unit_scale) {
+	length_m_sec_ = (end / unit_scale) - getOffset(unit_scale);
 }
 
 bool StopPointSM::isValid() const {
-	return isValid() && (length_sec_ > 0);
+	return isValid() && (length_m_sec_ > 0);
 }
--- vsrg/GameObjects/SM/Singular/Event/StopPointSM.h
+++ vsrg/GameObjects/SM/Singular/Event/StopPointSM.h
@@ -4,21 +4,21 @@
 // This doesn't really follow ScrollPoint implementation, so we'll use EventObj
 class StopPointSM : public EventObject
 {
-	StopPointSM(double offset_m_sec, double length_sec);
+	StopPointSM(double offset_m_sec, double length, double unit_scale = TimedObject::UnitScale::second);
 
 	// Returns =Seconds
 	std::string asNative() const override;
 
-	StopPointSM(double offset_m_sec, double length);
+	StopPointSM(double offset_m_sec, double length_m_sec);
 
-	double getLength() const;
-	void setLength(double length);
-	double getEnd() const;
-	void setEnd(double end);
+	double getLength(double unit_scale = TimedObject::UnitScale::second) const;
+	void setLength(double length, double unit_scale = TimedObject::UnitScale::second);
+	double getEnd(double unit_scale = TimedObject::UnitScale::second) const;
+	void setEnd(double end, double unit_scale = TimedObject::UnitScale::second);
 
 	bool isValid() const override;
 
 private:
-	double length_sec_;
+	double length_m_sec_; // Despite natively, in seconds, we use m_sec to be consistent
 };
 
