0d6ac6bf3c8a7be63765aef51ed2fc19fdaca718
Evening <johnchangqi@hotmail.com>
2019-09-30 22:02:09 +0800

    Set up necessary functions for StopPoint

--- vsrg/GameObjects/SM/Singular/Event/StopPointSM.cpp
+++ vsrg/GameObjects/SM/Singular/Event/StopPointSM.cpp
@@ -1,2 +1,25 @@
 #include "stdafx.h"
 #include "StopPointSM.h"
+
+StopPointSM::StopPointSM(double offset_m_sec, double length) :
+	EventObject(offset_m_sec), length_(length) {}
+
+double StopPointSM::getLength() const {
+	return length_;
+}
+
+void StopPointSM::setLength(double length) {
+	length_ = length;
+}
+
+double StopPointSM::getEnd() const {
+	return getOffset() + length_;
+}
+
+void StopPointSM::setEnd(double end) {
+	length_ = end - getOffset();
+}
+
+bool StopPointSM::isValid() const {
+	return isValid() && (length_ > 0);
+}
--- vsrg/GameObjects/SM/Singular/Event/StopPointSM.h
+++ vsrg/GameObjects/SM/Singular/Event/StopPointSM.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "BaseObjects/Singular/Event/EventObject.h"
+
+// This doesn't really follow ScrollPoint implementation, so we'll use EventObj
 class StopPointSM : public EventObject
 {
+	StopPointSM(double offset_m_sec, double length);
+
 	std::string asNative() const override;
+
+	double getLength() const;
+	void setLength(double length);
+	double getEnd() const;
+	void setEnd(double end);
+
+	bool isValid() const override;
+
+private:
+	double length_;
 };
 
