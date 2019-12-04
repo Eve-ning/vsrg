6c4662507bd798f6e595b112d23a3b0a40fff0be
Evening <johnchangqi@hotmail.com>
2019-12-05 00:41:26 +0800

    Implement fuzzy offset index matching

--- vsrg/BaseObjects/Special/TimingGridBase.cpp
+++ vsrg/BaseObjects/Special/TimingGridBase.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "TimingGridBase.h"
 
-TimingGridBase::TimingGridBase(const size_t measures, const size_t beats, const size_t snaps, const double offset_ms, const double bpm) :
+TimingGridBase::TimingGridBase(size_t measures, size_t beats, size_t snaps, double offset_ms, double bpm) :
 	tgm_v_(measures, TimingGridMeasure(beats, snaps, bpm)), offset_ms_(offset_ms) {}
 
 TimingGridBase::~TimingGridBase() {}
@@ -19,7 +19,14 @@ bool TimingGridBase::isEmpty() const {
 
 size_t TimingGridBase::size() const { return tgm_v_.size(); }
 
-std::vector<double> TimingGridBase::getBpmVector() const {
+
+
+double TimingGridBase::length() const {
+	double sum = 0.0;
+	for (const auto& tgm : tgm_v_) sum += tgm.length();
+	return sum;
+}
+
 std::vector<double> TimingGridBase::getBpm1DVector() const {
 	std::vector<double> bpm_v;
 	auto bpm_v_it = bpm_v.begin();
@@ -30,10 +37,6 @@ std::vector<double> TimingGridBase::getBpm1DVector() const {
 	return bpm_v;
 }
 
-double TimingGridBase::length() const {
-	double sum = 0.0;
-	for (const auto& tgm : tgm_v_) sum += tgm.length();
-	return sum;
 void TimingGridBase::setBpm1DVector(const std::vector<double>& bpm_v) {
 	auto bpm_start = bpm_v.begin();
 	auto bpm_end = bpm_v.begin();
@@ -48,26 +51,101 @@ void TimingGridBase::setBpm1DVector(const std::vector<double>& bpm_v) {
 
 std::vector<std::vector<double>> TimingGridBase::getBpm2DVector() const {
 	std::vector<std::vector<double>> bpm_2v;
-	for (const auto& tgm : tgm_v_) bpm_2v.push_back(tgm.getBpmVector());
+	for (const auto& tgm : tgm_v_) bpm_2v.push_back(tgm.getBpm1DVector());
 	return bpm_2v;
 }
 
 void TimingGridBase::setBpm2DVector(const std::vector<std::vector<double>>& bpm_2v) {
 	BOOST_ASSERT_MSG(bpm_2v.size() == size(), "Incorrect Size");
-	for (size_t i = 0; i < size(); i++) tgm_v_[i].setBpmVector(bpm_2v[i]);
+	for (size_t i = 0; i < size(); i++) tgm_v_[i].setBpm1DVector(bpm_2v[i]);
 }
+
+std::vector<TimingGridMeasure> TimingGridBase::getTimingGridMeasureVector() const { return tgm_v_; }
+void TimingGridBase::setTimingGridMeasureVector(const std::vector<TimingGridMeasure>& tgm_v) { tgm_v_ = tgm_v; }
 
 // Getting offset is slow when it's large
 // Will see which functions need a much faster access
-
-double TimingGridBase::getOffsetAt(const size_t measure_i, const size_t beat_i, const size_t snap_i) {
+double TimingGridBase::getOffset(const TimingGridIndex& index) {
 	double offset = offset_ms_;
-	for (size_t i = 0; i < measure_i; i++) { 
-		offset += tgm_v_[i].length(); 
-	}
-	for (size_t i = 0; i < beat_i; i++) {
-		offset += tgm_v_[measure_i][i].length();
-	}
-	offset += tgm_v_[measure_i][beat_i].snapLength() * snap_i;
+	for (size_t i = 0; i < index.measure; i++) offset += tgm_v_[i].length(); 
+	for (size_t i = 0; i < index.beat; i++)    offset += tgm_v_[index.measure][i].length();
+	offset += tgm_v_[index.measure][index.beat].snapLength() * index.snap;
 	return offset;
+}
+
+double TimingGridBase::getOffset(size_t measure, size_t beat, size_t snap) {
+	return getOffset(TimingGridIndex(measure, beat, snap));
+}
+
+TimingGridIndex TimingGridBase::getIndex(const double& offset) {
+	// This is an offset search algorithm
+	double offset_i = 0.0;
+	TimingGridIndex index = TimingGridIndex(0, 0, 0);
+
+	// These are save states, usually the most optimized would be
+	// with measures, then beats, then snaps 
+	TimingGridIndex index_m, index_b, index_s;
+	double offset_m, offset_b, offset_s;
+
+	try {
+		// By Measures
+		auto measure_i = tgm_v_.begin();
+		while (offset_i + measure_i->length() <= offset) { // If adding a measure length exceeds
+			offset_i += measure_i->length();
+			measure_i++;
+			index.measure++;
+		}
+		// Save measure on next state
+		index_m = index;
+		index_m.measure++;
+		offset_m = offset_i + measure_i->length();
+
+		// By Beats
+		auto tgb_v = measure_i->getTimingGridBeatVector();
+		auto beat_i = tgb_v.begin();
+		while (offset_i + beat_i->length() <= offset) { // If adding a beat length exceeds 
+			offset_i += beat_i->length();
+			beat_i++;
+			index.beat++;
+		}
+		// Save beat on next state
+		index_b = index;
+		index_b.beat++;
+		offset_b = offset_i + beat_i->length();
+
+		// By Snaps
+		double snap_length = beat_i->snapLength();
+		index.snap = (size_t)(std::round((offset - offset_i) / snap_length));
+
+		// Save snap
+		index_s = index;
+		offset_s = offset_i + beat_i->snapLength() * index.snap;
+	}
+	catch ([[maybe_unused]] std::out_of_range & e) {
+		throw std::out_of_range("Offset doesn't exist.");
+	}
+	
+	// We compare all states to see which one works best.
+	// If there's a tie, we take the most optimized one.
+
+	double error_s = abs(offset - offset_s);
+	double error_b = abs(offset - offset_b);
+	double error_m = abs(offset - offset_m);
+	constexpr double epsilon = std::numeric_limits<double>::epsilon();
+
+	if (error_m - error_b < epsilon && error_m - error_s < epsilon) { return index_m; }
+	else if (error_b - error_s < epsilon) { return index_b;  }
+	else { return index_s; }
+}
+
+inline TimingGridSnap& TimingGridBase::getSnap(const TimingGridIndex& index) {
+	return tgm_v_[index.measure][index.beat][index.snap];
+}
+
+inline void TimingGridBase::setSnap(const TimingGridIndex& index, const std::vector<SPtrHitObject>& ho_v) {
+	getSnap(index).setHitObjectVector(ho_v);
+}
+
+inline void TimingGridBase::pushSnap(const TimingGridIndex& index, const SPtrHitObject& ho) {
+	getSnap(index).push_back(ho);
 }
--- vsrg/BaseObjects/Special/TimingGridBase.h
+++ vsrg/BaseObjects/Special/TimingGridBase.h
@@ -1,15 +1,34 @@
 #pragma once
 #include "BaseObjects/Special/TimingGridMeasure.h"
+#include "BaseObjects/Multiple/EventObjectVector.h"
+#include "BaseObjects/Multiple/HitObjectVector.h"
+#include "BaseObjects/Singular/Event/TimingPoint.h"
+
+// Temp include for intellisense
+#include "GameObjects/SM/Singular/Event/TimingPointSM.h"
 
 class TimingGridBase
 {
 public:
 	// If measures always contain the same amount of beats
-	TimingGridBase(const size_t measures,
-				   const size_t beats,
-				   const size_t snaps,
-				   const double offset_ms = 0.0,
-			       const double bpm = 0.0);	
+	TimingGridBase(size_t measures,
+				   size_t beats,
+				   size_t snaps,
+				   double offset_ms = 0.0,
+			       double bpm = 0.0);
+
+	template<class TimingPointClass>
+	TimingGridBase(const SPtrEventObjectVector& eo_v,
+				   const SPtrHitObjectVector& ho_v) {
+		auto tp_v = eo_v->getClassOnly<TimingPointClass>();
+		
+		// Sorts the vector
+		std::sort(tp_v.begin(), tp_v.end()); 
+			//[](const TimingPointClass& i, const TimingPointClass& j) {
+			//	return i.getOffset() < j.getOffset();
+			//});
+		
+	}
 	~TimingGridBase();
 
 	TimingGridMeasure& operator [](size_t i);
@@ -17,7 +36,6 @@ public:
 	bool isEmpty() const;
 
 	size_t size() const;
-	std::vector<double> getBpmVector() const;
 	double length() const;
 
 	std::vector<double> getBpm1DVector() const;
@@ -25,13 +43,26 @@ public:
 	std::vector<std::vector<double>> getBpm2DVector() const;
 	void setBpm2DVector(const std::vector<std::vector<double>>& bpm_2v);
 
+	std::vector<TimingGridMeasure> getTimingGridMeasureVector() const;
+	void setTimingGridMeasureVector(const std::vector<TimingGridMeasure>& tgm_v);
+
 	// Getting offset is slow when it's large
 	// Will see which functions need a much faster access
-	double getOffsetAt(const size_t measure_i,
-					   const size_t beat_i,
-					   const size_t snap_i);
+	double getOffset(const TimingGridIndex& index);
+	double getOffset(size_t measure,
+					 size_t beat,
+				     size_t snap);
+
+	TimingGridIndex getIndex(const double& offset);
+	TimingGridSnap& getSnap(const TimingGridIndex& index);
+	void setSnap(const TimingGridIndex& index,
+				 const std::vector<SPtrHitObject>& ho_v);
+	void pushSnap(const TimingGridIndex& index,
+				  const SPtrHitObject& ho);
+
 
 private:
+
 	std::vector<TimingGridMeasure> tgm_v_;
 	double offset_ms_;
 };
